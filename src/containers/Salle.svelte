a8b46628b70648472c75f5250a4b98f7e4d5d322
remabd <remiabdallah3@gmail.com>
2026-03-15 10:00:27 +0100

    feat(feat-salle): fix filter function

--- src/containers/Salle.svelte
+++ src/containers/Salle.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
     import { onMount } from 'svelte';
     import type { Salle, SalleWithReservations } from '../models/salle.entity';
+    import type { Reservation } from '../models/reservation.entity';
+
     import SalleController from '../controllers/salle.controller';
     import ReservationController from '../controllers/reservation.controller';
+
     import DatePicker from '../components/DatePicker.svelte';
 
     const salleController = new SalleController();
@@ -12,18 +15,18 @@
 
     let capacity = $state(0);
     let computers = $state(0);
-    let teacherComputer = $state<boolean | null>(null);
-    let airCool = $state<boolean | null>(null);
-
+    let teacherComputer = $state<boolean>(false);
+    let airCool = $state<boolean>(false);
     let selected = $state<string | null>();
 
     let displayed = $derived(
         salles.filter(
-            (salle: Salle) =>
+            (salle: SalleWithReservations) =>
                 salle.capacity >= capacity &&
                 salle.computers >= computers &&
-                (teacherComputer === null || salle.teacherComputer === teacherComputer) &&
-                (airCool === null || salle.aircool === airCool)
+                (!teacherComputer || (teacherComputer && salle.teacherComputer)) &&
+                (!airCool || (airCool && salle.aircool)) &&
+                !salle.reservations.some((r: Reservation) => r.date === selected)
         )
     );
 
@@ -58,7 +61,7 @@
 
         <fieldset>
             <legend>Ordinateur professeur</legend>
-            {#each [true, false, null] as option}
+            {#each [true, false] as option}
                 <label>
                     <input
                         type="radio"
@@ -66,17 +69,17 @@
                         value={option}
                         bind:group={teacherComputer}
                     />
-                    {option === null ? 'Peu importe' : option ? 'Oui' : 'Non'}
+                    {option ? 'Oui' : 'Non'}
                 </label>
             {/each}
         </fieldset>
 
         <fieldset>
             <legend>Climatisation</legend>
-            {#each [true, false, null] as option}
+            {#each [true, false] as option}
                 <label>
                     <input type="radio" name="airCool" value={option} bind:group={airCool} />
-                    {option === null ? 'Peu importe' : option ? 'Oui' : 'Non'}
+                    {option ? 'Oui' : 'Non'}
                 </label>
             {/each}
         </fieldset>
